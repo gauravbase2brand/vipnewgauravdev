a3b64da1eacb774a30562a231ad03c1bc9c969f5
techbase2brand <techbase2brand@gmail.com>
2025-08-07 14:54:46 +0530

    changes done

--- src/app/Shared/TabNumbers/TabNumbers.js
+++ src/app/Shared/TabNumbers/TabNumbers.js
@@ -180,7 +180,6 @@ const TabNumbers = () => {
               description={item?.description}
             />
             {index === 0 && selectedCategoryTop?.items?.length > 0 && (
-              <>
               <NetedCategory
                 viewingProduct={viewingProduct}
                 productIndexes={productIndexes}
@@ -190,23 +189,13 @@ const TabNumbers = () => {
                 setViewingProduct={setViewingProduct}
                 setSelectedCategory={setSelectedCategoryTop}
               />
-              <NetedCategory
-                viewingProduct={viewingProduct}
-                productIndexes={productIndexes}
-                selectedCategory={selectedCategoryMiddle}
-                categories={categoriesMiddle}
-                setProductIndexes={setProductIndexes}
-                setViewingProduct={setViewingProduct}
-                setSelectedCategory={setSelectedCategoryMiddle}
-              />
-              </>
             )}
 
             {index === 1 && <ClientVideo />}
             {index === 2 && <FamilyPack counter={4} />}
             {index === 3 && <Benefits />}
             {index === 4 && <QRVipApp />}
-            {/* {(index === 5 && selectedCategoryMiddle?.items?.length > 0) && (
+            {(index === 5 && selectedCategoryMiddle?.items?.length > 0) && (
               <NetedCategory
                 viewingProduct={viewingProduct}
                 productIndexes={productIndexes}
@@ -216,7 +205,7 @@ const TabNumbers = () => {
                 setViewingProduct={setViewingProduct}
                 setSelectedCategory={setSelectedCategoryMiddle}
               />
-            )} */}
+            )}
             {index === 6 && <FamilyPack counter={3} />}
             {index === 7 && !isMobile && (
               <CityHowGetVipNumber
